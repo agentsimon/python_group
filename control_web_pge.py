1dc1525316092d208e781ae5dccd2f477f1abea6
agentsimon <31639346+agentsimon@users.noreply.github.com>
2023-02-18 15:55:10 +0700

    Add files via upload
    Selenium web controller

---
+++ control_web_pge.py
@@ -0,0 +1,99 @@
+# Import the pandas library
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+#time.sleep(10)
+# delay time
+web_page_time = 2
+# processing time
+process_time = 40
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+pd.options.mode.chained_assignment = None  
+
+# default='warn'# Set up the correct values
+def set_up():
+# Go to the settings and set up values
+    settings_button = browser.find_element("id", "tab-settings")
+    settings_button.click()
+    time.sleep(web_page_time)
+    select = Select(browser.find_element("id",'vram_usage_level'))
+    time.sleep(web_page_time)
+    select.select_by_value('low')
+    time.sleep(web_page_time)
+# Turn on the auto save images toggle
+    settings_save = browser.find_element(By.XPATH, '//*[@id="save_to_disk"]')
+    
+    time.sleep(web_page_time)
+# Select the auto save option
+    if settings_save.is_selected() == False:
+        print("We have the settings save button")
+        print("Location: ", settings_save.location)
+        browser.execute_script("arguments[0].click();", settings_save)
+
+    time.sleep(web_page_time)
+    #elem=WebDriverWait(browser,50).until(EC.visibility_of_element_located((By.ID, "save_to_disk")))
+    #browser.execute_script("arguments[0].scrollIntoView();",elem)
+    #elem.click()
+# Go to the main window and set up the values
+    settings_button = browser.find_element("id", "tab-main")
+    settings_button.click()
+    time.sleep(web_page_time)
+ # select the prompt and clear it
+    prompt_name = browser.find_element("id", "prompt")
+    browser.find_element('id', "prompt").clear()
+    time.sleep(web_page_time)
+    print("Cleared")
+# Select the model
+    input = browser.find_element("id","editor-settings")
+    time.sleep(web_page_time)
+    input.click()
+    time.sleep(web_page_time)
+    settings_button2 = browser.find_element("id", "stable_diffusion_model")
+    time.sleep(web_page_time)
+# identify dropdown for model and select
+    select = Select(WebDriverWait(browser, 2).until(EC.element_to_be_clickable((By.ID, "stable_diffusion_model"))))
+    select.select_by_visible_text("sd-v1-4")
+ # identify dropdown for sampler and select
+    select = Select(WebDriverWait(browser, 2).until(EC.element_to_be_clickable((By.ID, "sampler_name"))))
+    select.select_by_visible_text("Euler")
+# identify dropdown for image type and select
+    select = Select(WebDriverWait(browser, 2).until(EC.element_to_be_clickable((By.ID, "output_format"))))
+    select.select_by_visible_text("png")
+# Select the fix face button use_face_correction
+# Turn on the auto save images toggle
+    settings_face = browser.find_element(By.XPATH, '//*[@id="use_face_correction"]')
+    if settings_face.is_selected() == False:
+        print("We have the settings face correction")
+        print("Location: ", settings_face.location)
+        browser.execute_script("arguments[0].click();", settings_face)
+
+
+# Main Body
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+browser.get("http://localhost:9000/")
+#browser.get('http://www.agentsimon.co.uk/')
+browser.maximize_window()
+
+set_up()
+prompt_name ="walking the streets of Hanoi"
+prompt_name = prompt_name + " 2023"
+print("Prompt is ", prompt_name)
+ # Finding the search field by id
+input = browser.find_element("id","prompt")
+input.send_keys(prompt_name)
+# clicking on the Make image button
+button = browser.find_element("id","makeImage")
+button.click()
+time.sleep(process_time)
+print("We have finished")
+browser.close()
