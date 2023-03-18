19a0d151e0b08cfc9bc772e7809d484b0aa6e471
agentsimon <31639346+agentsimon@users.noreply.github.com>
2023-03-18 13:56:30 +0700

    Update control_web_pge.py
    Moved the prompt to line 38 and added a time print

--- control_web_pge.py
+++ control_web_pge.py
@@ -19,16 +19,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
-
+from datetime import datetime
 import time
 import os
 import subprocess
 import shlex
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Start time is ", current_time)
 # delay time
 web_page_time = 1
 # processing time
 process_time = 35
 
+# Put the prompt here as prompt name
+prompt_name =""
+fine_tune =""
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 pd.options.mode.chained_assignment = None  
@@ -43,6 +51,11 @@ def set_up():
     select = Select(browser.find_element("id",'vram_usage_level'))
     time.sleep(web_page_time)
     select.select_by_value('low')
+    time.sleep(web_page_time)
+# Turn of the meta data saving 
+    select = Select(browser.find_element("id", 'metadata_output_format'))
+    time.sleep(web_page_time)
+    select.select_by_value('none')
     time.sleep(web_page_time)
 # Turn on the auto save images toggle
     settings_save = browser.find_element(By.XPATH, '//*[@id="save_to_disk"]') 
@@ -73,7 +86,7 @@ def set_up():
     prompt_clear = browser.find_element("id", "negative_prompt_handle")
     time.sleep(web_page_time)
     prompt_clear.click()
-    prompt_name_negative = "worst quality, low quality, normal quality, low resolution), blurry, text, artist name, signature, multiple views, out of frame, thumbnail, preview, obscured, distorted, blurry, blurred, out of focus low-res, low resolution, bad quality, artifacts, low detail, no detail, corrupted"
+    prompt_name_negative = "hat,wand, fuuin no tsue, holding, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, bad feet"
     input = browser.find_element("id","negative_prompt")
     input.send_keys(prompt_name_negative)
     time.sleep(web_page_time)
@@ -81,8 +94,6 @@ def set_up():
     input = browser.find_element("id","editor-settings")
     time.sleep(web_page_time)
     input.click()
-    time.sleep(web_page_time)
-    settings_button2 = browser.find_element("id", "stable_diffusion_model")
     time.sleep(web_page_time)
 # identify Model dropdown for model and select V1-5-pruned
     pick_model = browser.find_element("id", "stable_diffusion_model")
@@ -93,10 +104,21 @@ def set_up():
         if option.text == "v1-5-pruned":
             option.click()
             break
+# identify VAE Model dropdown for model and select V1-5-pruned
+    pick_model = browser.find_element("id", "vae_model")
+    time.sleep(web_page_time)
+    pick_model.click()
+    options = browser.find_elements(By.XPATH, "//li[@style='display: list-item;']")
+    for option in options:
+        if option.text == "euler_a":
+            option.click()
+            break
  
 # Turn on fix faces toggle
-    settings_save = browser.find_element(By.XPATH, '//*[@id="use_face_correction"]') 
+    settings_save = browser.find_element(By.XPATH, '//*[@id="editor-settings-entries"]/div[2]/ul/li[3]/div[1]/label') 
+    settings_save.click()
     time.sleep(web_page_time)
+
 # identify dropdown for image type and select
     select = Select(WebDriverWait(browser, 2).until(EC.element_to_be_clickable((By.ID, "output_format"))))
     select.select_by_visible_text("jpeg")
@@ -114,11 +136,12 @@ def set_up():
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 browser.get("http://localhost:9000/")
 browser.maximize_window()
+
 # Run the set up function
 set_up()
 
 # Load the CSV file into a dataframe
-df = pd.read_csv("/home/simonhyde/Documents/stable-diffusion-ui-linux/stable-diffusion-ui/Countries_and_Cities.csv",  engine ='python')
+df = pd.read_csv("/home/simon/Documents/Python/scrape/Countries_and_Cities.csv",  engine ='python')
 
 # Create a new column in the dataframe
 df['Country_Capital_Model'] = ""
@@ -129,17 +152,17 @@ for col in df.columns:
 count_row = df.shape[0]  # Gives number of rows
 df['Country_Capital_Model'] = ""
 print("Number of rows ",count_row)
-# Put the prompt here as prompt name
-prompt_name ="tea house"
+
 print("The prompt is ", prompt_name)
 time.sleep(3)
 file_name = prompt_name
-for model_image in range(10):
+for model_image in range(count_row):
     print(model_image)
     print("Here", df.iloc[model_image,0], " ", df.iloc[model_image,1])
     prompt_name2 =  str(df.iloc[model_image,0]), str(df.iloc[model_image,1]) +" " +prompt_name 
     complete = ' '.join(prompt_name2)  
     print("Prompt is ", complete)
+# Add the name of the prompt to the column Title
     df.at[model_image,'Title'] = complete
  # Finding the search field by id
     input = browser.find_element("id","prompt")
@@ -149,15 +172,6 @@ for model_image in range(10):
     button.click()
 # Wait for the image to be made which will be auto saved
     time.sleep(process_time)
-    # Set only show 4 images only once
-    if model_image == 1:
-        input = browser.find_element("id","madrang-plugin-keep-max")
-        input.clear()
-        value_images = 4
-        input.send_keys(value_images)
-        time.sleep(web_page_time)
-    else:
-        pass
      # select the prompt and clear it
     #Clear the prompt window
     browser.find_element('id', "prompt").clear()
@@ -165,23 +179,26 @@ for model_image in range(10):
     print("Cleared")
 
 
-# Add the get created images form the folder and add the file names to a new column called Images
-subfolders = [ f.path for f in os.scandir('/home/simonhyde/Stable Diffusion UI/') if f.is_dir() ]
+# AFind the colder with the images in
+subfolders = [ f.path for f in os.scandir('/home/simon/Stable Diffusion UI') if f.is_dir() ]
 print("Folders are ", subfolders)
 folder_to_look_in = max(subfolders,key=os.path.getctime)
 print("Name of folder is", folder_to_look_in)
 
-
+# Get the files in the folder
 csv_files = [file for file in os.listdir(folder_to_look_in) if file.endswith('.jpeg')]
-print("Unsorted list ",csv_files)
 csv_files2 =sorted(csv_files)
 print("Sorted list of files",csv_files2)
-# Loop through the rows in the dataframe
+# Loop through the rows in the dataframe and add the file names to the column called Image
 row_number =0
 for model_image in range(len(csv_files2)):   
     df.at[row_number,'Image'] = csv_files2[model_image]
     row_number += 1
+
+# Save the csv file
 file_name = file_name +".csv"
 df.to_csv(file_name, sep=',')
 print("We have finished ", file_name)
+current_time = now.strftime("%H:%M:%S")
+print("End time is ", current_time)
 browser.close()
